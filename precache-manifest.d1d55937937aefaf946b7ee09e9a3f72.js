29c1bb8a36bee0f1525593eb34b121301aa1f693
Florian Gérard <kingdomflo16@gmail.com>
2020-10-10 10:09:22 +0200

    Updates

---
+++ precache-manifest.d1d55937937aefaf946b7ee09e9a3f72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841aaab37b4ef014c347d35b313f8273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dce9bdfd3c4512196c8b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdfc518e8c886d08cf43",
+    "url": "/static/js/2.5f4523cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f4523cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce9bdfd3c4512196c8b",
+    "url": "/static/js/main.f13e80be.chunk.js"
+  },
+  {
+    "revision": "e09751e01fd2c52ac4a8",
+    "url": "/static/js/runtime-main.5ffb660f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
