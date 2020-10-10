55c7fc52bb3f740fb4bd5de59e4eee4fc983a2c7
Florian Gérard <kingdomflo16@gmail.com>
2020-10-10 10:16:06 +0200

    re test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacte
+          Learn Reacteee
         </a>
       </header>
     </div>
