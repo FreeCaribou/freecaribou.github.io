21a0b068e4e20f2e002963f41e2024f863d4562c
Florian Gérard <kingdomflo16@gmail.com>
2020-10-10 10:24:01 +0200

    router test

---
+++ build/precache-manifest.64b322da1a69293490401af962f87b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f815dd289d220baf90c9d45e5f48dfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23d0316029c361de438e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dff2a923a410a9cd11d4",
+    "url": "/static/js/2.32ac656c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.32ac656c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d0316029c361de438e",
+    "url": "/static/js/main.6358de69.chunk.js"
+  },
+  {
+    "revision": "e09751e01fd2c52ac4a8",
+    "url": "/static/js/runtime-main.5ffb660f.js"
+  }
+]);
