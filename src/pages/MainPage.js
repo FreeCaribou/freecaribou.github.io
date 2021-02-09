e9897af84e973e669ed0a6fe26aebdb8572b34a7
Florian Gérard <kingdomflo16@gmail.com>
2021-02-09 10:36:18 +0100

    testouille

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useTranslation } from "react-i18next";
 import {
   HashRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-import { Container, Menu } from "semantic-ui-react";
+import { Container, Dropdown, Flag, Menu } from "semantic-ui-react";
+import LanguageContext from "../hooks/useLanguageContext";
 import CareerPage from "./CareerPage";
 import HomePage from "./HomePage";
 import ProjectPage from "./ProjectPage";
 
 export default function MainPage() {
+
+  const { t, i18n } = useTranslation();
+  const [currentLanguage, setCurrentLanguage] = useContext(LanguageContext);
+
+  const languages = [
+    {
+      key: 'en',
+      value: 'en',
+      text: 'English',
+      image: { avatar: true, src: '/flag/en.png' }
+    },
+    {
+      key: 'fr',
+      value: 'fr',
+      text: 'French',
+      image: { avatar: true, src: '/flag/fr.png' }
+    },
+    {
+      key: 'no',
+      value: 'no',
+      text: 'Norsk',
+      image: { avatar: true, src: '/flag/no.png' }
+    },
+  ]
+
+  const languageChange = (e, d) => {
+    console.log(e, d);
+    setCurrentLanguage(d.value);
+  }
+
   return (
     <Router>
       <div>
 
         <Menu color={'blue'} inverted fixed='top'>
           <Container>
+
             <Menu.Item color={'orange'}>
               <Link to="/">Home</Link>
             </Menu.Item>
@@ -26,6 +59,17 @@ export default function MainPage() {
             <Menu.Item color={'orange'}>
               <Link to="/career">Career</Link>
             </Menu.Item>
+
+            <Menu.Item position='right'>
+              <Dropdown placeholder={t('General.Language')} floating>
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={(e, d) => languageChange(e, d)} value="en" text="English" image={{ avatar: true, src: '/flag/en.png' }} />
+                  <Dropdown.Item onClick={(e, d) => languageChange(e, d)} value="fr" text="Français" image={{ avatar: true, src: '/flag/fr.png' }} />
+                  <Dropdown.Item onClick={(e, d) => languageChange(e, d)} value="no" text="Norsk" image={{ avatar: true, src: '/flag/no.png' }} />
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu.Item>
+
           </Container>
         </Menu>
 
